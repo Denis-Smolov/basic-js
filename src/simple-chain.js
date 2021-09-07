3302bcd4e8bf653b6f57b7838bb704bd526e84ac
Смолов Денис <smolovda@it2g.ru>
2021-09-07 14:05:25 +0300

    simple-chain-11;

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -12,102 +12,59 @@ let chainLinks;
 export default {
     chainLinks,
 
-    constructor() {
-        this.chainLinks = [];
+    init() {
+        if (chainLinks === undefined) {
+            chainLinks = [];
+        }
+        return chainLinks;
     },
 
     getLength() {
-        return this.chainLinks.length;
+        this.init();
+        return chainLinks.length;
     },
 
     addLink(value) {
-        this.chainLinks.push(value === undefined ? '*' : value);
+        this.init();
+        chainLinks.push(value === undefined ? '*' : value);
         return this;
     },
 
     removeLink(position) {
+        this.init();
         if (position === null || position === undefined || Number.isNaN(position) || !Number.isInteger(position)
-            || position < 0 || position > this.getLength()) {
+            || position <= 0 || position > this.getLength()) {
             throw new Error(ERROR_TEXT);
         }
-        this.chainLinks.splice(position - 1, 1);
+        chainLinks.splice(position - 1, 1);
         return this;
     },
 
     reverseChain() {
+        this.init();
         let reversed = [];
 
         for (let i = this.getLength() - 1; i >= 0; i--) {
-            reversed = reversed.concat(this.chainLinks[i]);
+            reversed = reversed.concat(chainLinks[i]);
         }
-        this.chainLinks = reversed;
+        chainLinks = reversed;
         return this;
     },
 
     finishChain() {
+        this.init();
         let result = '';
         for (let i = 0; i < this.getLength(); i++) {
-            let element = this.chainLinks[i];
+            let element = chainLinks[i];
             if (i !== 0) {
                 result = result.concat(LINK_1);
             }
             result = element === '*' ? result.concat(LINK_2).concat(LINK_3) : result.concat(LINK_2).concat(element).concat(LINK_4);
         }
-        this.chainLinks = [];
+        chainLinks = [];
         return result;
     }
 }
-/**//*
-
-class ChainMaker {
-    chainLinks;
-
-    constructor() {
-        this.chainLinks = [];
-    }
-
-    getLength() {
-        return this.chainLinks.length;
-    }
-
-    addLink(value) {
-        this.chainLinks.push(value === undefined ? '*' : value);
-        return this;
-    }
-
-    removeLink(position) {
-        if (position === null || position === undefined || Number.isNaN(position) || !Number.isInteger(position)
-            || position < 0 || position > this.getLength()) {
-            throw new Error(ERROR_TEXT);
-        }
-        this.chainLinks.splice(position - 1, 1);
-        return this;
-    }
-
-    reverseChain() {
-        let reversed = [];
-
-        for (let i = this.getLength() - 1; i >= 0; i--) {
-            reversed = reversed.concat(this.chainLinks[i]);
-        }
-        this.chainLinks = reversed;
-        return this;
-    }
-
-    finishChain() {
-        let result = '';
-        for (let i = 0; i < this.getLength(); i++) {
-            let element = this.chainLinks[i];
-            if (i !== 0) {
-                result = result.concat(LINK_1);
-            }
-            result = element === '*' ? result.concat(LINK_2).concat(LINK_3) : result.concat(LINK_2).concat(element).concat(LINK_4);
-        }
-        this.chainLinks = [];
-        return result;
-    }
-}
-*/
 
 // let chainMaker = new ChainMaker();
 // console.log(chainMaker.addLink().addLink(2).addLink().finishChain()); // => '( )~~( 2 )~~( )'
@@ -117,4 +74,4 @@ class ChainMaker {
 // console.log(chainMaker.getLength());
 // chainMaker.finishChain();
 // console.log(chainMaker.addLink(1).addLink(2).reverseChain().addLink(3).finishChain()); // => '( 2 )~~( 1 )~~( 3 )'
-// console.log(chainMaker.addLink(1).addLink(2).removeLink(9).addLink(3).finishChain());
+// console.log(chainMaker.addLink(1).addLink(2).removeLink(9).addLink(3).finishChain()); // ERROR
